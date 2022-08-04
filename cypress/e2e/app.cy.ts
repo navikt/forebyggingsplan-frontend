e2ffe991741a2adc26815253e41a90432ad0a28e
Stefan A. Engelien <stefan.andreas.engelien@nav.no>
2022-08-04 15:43:20 +0200

    Lagt til en aktivitetsoversikt, med hardkodet innhold
    Co-authored-by: Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
    Co-authored-by: Christian Klem <christian.klem@nav.no>

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,6 +1,30 @@
 describe('Navigasjon', () => {
-  it('vi har definert tittel', () => {
-    cy.visit('/')
-    cy.get("title").contains("Forebyggingsplan")
-  })
+
+    it('vi har definert sidetittel', () => {
+        cy.visit('/')
+        cy.get("title").contains("Forebyggingsplan")
+    })
+
+    it('vi har definert overskrift for aktivitetsoversikten', () => {
+        cy.visit('/')
+        cy.get("h1").contains("Forebyggingsplan")
+    })
+
+    it('vi har en tabell som viser alle aktvitetsmalene', () => {
+        cy.visit('/')
+        cy.get("#aktivitetstabell th")
+            .should(($headers) => {
+                expect($headers).to.have.length(1)
+                expect($headers.eq(0)).to.contain('Alle aktiviteter')
+            })
+
+        cy.get("#aktivitetstabell td")
+            .should(($datacells) => {
+                expect($datacells).to.have.length(3)
+                expect($datacells.eq(0)).to.contain('Aktivitet 1')
+                expect($datacells.eq(1)).to.contain('Aktivitet 2')
+                expect($datacells.eq(2)).to.contain('Aktivitet 3')
+            })
+    })
+
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,51 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {Table} from '@navikt/ds-react'
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Forebyggingsplan</title>
-        <meta name="description" content="Et verktøy der arbeidsgivere kan jobbe med å forebygge sykefravær i sin virksomhet" />
-        <link rel="icon" href="https://nav.no/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Forebyggingsplan</title>
+                <meta name="description"
+                      content="Et verktøy der arbeidsgivere kan jobbe med å forebygge sykefravær i sin virksomhet"/>
+                <link rel="icon" href="https://nav.no/favicon.ico"/>
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Forebyggingsplan
+                </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+                <Table size="medium" id={"aktivitetstabell"}>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell scope="col">
+                                Alle aktiviteter
+                            </Table.HeaderCell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.DataCell scope="col">
+                                Aktivitet 1
+                            </Table.DataCell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.DataCell scope="col">
+                                Aktivitet 2
+                            </Table.DataCell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.DataCell scope="col">
+                                Aktivitet 3
+                            </Table.DataCell>
+                        </Table.Row>
+                    </Table.Header>
+                </Table>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            </main>
         </div>
-      </main>
-    </div>
-  )
+    )
 }
 
 export default Home
