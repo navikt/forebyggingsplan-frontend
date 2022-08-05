4aae1cf4d325b4a2d3decf4da1c06e5573fd1aaf
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-08-05 12:17:22 +0200

    Sett opp prettier
    Co-authored-by: Stefan A. Engelien <stefan.andreas.engelien@nav.no>
    Co-authored-by: Christian Klem <christian.klem@nav.no>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,42 @@
-import type {NextPage, NextPageContext} from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import {Aktivitet} from "../src/types/Aktivitet";
-import {Aktivitetsmaler} from "../src/components/Aktivitetsmaler";
+import type { NextPage, NextPageContext } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { Aktivitet } from "../src/types/Aktivitet";
+import { Aktivitetsmaler } from "../src/components/Aktivitetsmaler";
 
 const mockSvar: Aktivitet[] = [
-    {tittel: "Aktivitet 1"},
-    {tittel: "Aktivitet 2"},
-    {tittel: "Aktivitet 3"},
-]
+    { tittel: "Aktivitet 1" },
+    { tittel: "Aktivitet 2" },
+    { tittel: "Aktivitet 3" },
+];
 
 export async function getServerSideProps(context: NextPageContext) {
     return {
         props: {
-            aktiviteter: mockSvar
+            aktiviteter: mockSvar,
         },
     };
 }
 
-const Home: NextPage<{ aktiviteter: Aktivitet[] }> = ({aktiviteter}) => {
+const Home: NextPage<{ aktiviteter: Aktivitet[] }> = ({ aktiviteter }) => {
     return (
         <div className={styles.container}>
             <Head>
                 <title>Forebyggingsplan</title>
-                <meta name="description"
-                      content="Et verktøy der arbeidsgivere kan jobbe med å forebygge sykefravær i sin virksomhet"/>
-                <link rel="icon" href="https://nav.no/favicon.ico"/>
+                <meta
+                    name="description"
+                    content="Et verktøy der arbeidsgivere kan jobbe med å forebygge sykefravær i sin virksomhet"
+                />
+                <link rel="icon" href="https://nav.no/favicon.ico" />
             </Head>
 
             <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Forebyggingsplan
-                </h1>
+                <h1 className={styles.title}>Forebyggingsplan</h1>
 
                 <Aktivitetsmaler aktiviteter={aktiviteter} />
             </main>
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
