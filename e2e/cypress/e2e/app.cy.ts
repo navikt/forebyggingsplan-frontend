92c9a5cca926c6b705ea82a927992d4f9a24ee81
Thomas Dufourd <thomas.dufourd@gmail.com>
2022-08-16 16:23:17 +0200

    Teste at button 'Legg til' finnes på oversikt
    Co-authored-by: Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>

--- e2e/cypress/e2e/app.cy.ts
+++ e2e/cypress/e2e/app.cy.ts
@@ -12,21 +12,23 @@ describe("Navigasjon", () => {
     it("vi har en tabell som viser alle aktvitetsmalene", () => {
         cy.visit("/");
         cy.get("#aktivitetstabell thead th").should(($headers) => {
-            expect($headers).to.have.length(1);
+            expect($headers).to.have.length(2);
+            console.log("Headers", $headers);
             expect($headers.eq(0)).to.contain("Alle aktiviteter");
+            expect($headers.eq(1)).to.contain("Legg til");
         });
 
         cy.get("#aktivitetstabell tbody td").should(($datacells) => {
-            expect($datacells).to.have.length(4);
+            expect($datacells).to.have.length(8);
             expect($datacells.eq(0)).to.contain(
                 "Pilotering av medarbeidersamtalen"
             );
-            expect($datacells.eq(1)).to.contain("Kartleggingsmøte med ansatt");
-            expect($datacells.eq(2)).to.contain("Sinnemestring");
-            expect($datacells.eq(3)).to.contain(
+            expect($datacells.eq(2)).to.contain("Kartleggingsmøte med ansatt");
+            expect($datacells.eq(4)).to.contain("Sinnemestring");
+            expect($datacells.eq(6)).to.contain(
                 "Hvordan ta den vanskelige praten?"
             );
-            expect($datacells).to.contain("button")
+            expect($datacells.eq(1).children('.navds-button')).to.exist;
         });
     });
 });
