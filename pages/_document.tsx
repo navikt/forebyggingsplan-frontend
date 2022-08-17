1704d2e9a02e8ffbab60777dc269bd7b1ddf4474
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-08-17 15:29:51 +0200

    Legger til dekoratøren i appen, foreløbig kun i dev-mode
    Co-authored-by: Thomas Dufourd <thomas.dufourd@gmail.com>

---
+++ pages/_document.tsx
@@ -0,0 +1,53 @@
+import {
+    DocumentContext,
+    DocumentInitialProps,
+    Html,
+    Head,
+    Main,
+    default as Document,
+    NextScript,
+} from "next/document";
+import {
+    Components,
+    fetchDecoratorReact,
+} from "@navikt/nav-dekoratoren-moduler/ssr";
+
+interface Props {
+    Dekoratør: Components;
+}
+
+export default class MyDocument extends Document<Props> {
+    static async getInitialProps(
+        ctx: DocumentContext
+    ): Promise<DocumentInitialProps & Props> {
+        const initialProps = await Document.getInitialProps(ctx);
+
+        const Dekoratør = await fetchDecoratorReact({
+            env: "dev",
+            chatbot: false,
+            context: "arbeidsgiver",
+            breadcrumbs: [],
+        });
+
+        return { ...initialProps, Dekoratør };
+    }
+
+    render(): JSX.Element {
+        const { Dekoratør } = this.props;
+
+        return (
+            <Html lang="no">
+                <Head>
+                    <Dekoratør.Styles />
+                </Head>
+                <body>
+                    <Dekoratør.Header />
+                    <Main />
+                    <Dekoratør.Footer />
+                    <Dekoratør.Scripts />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,17 @@ import styles from "../styles/Home.module.css";
 import { Aktivitet } from "../src/types/Aktivitet";
 import { Aktivitetsmaler } from "../src/components/Aktivitetsmaler";
 import { FOREBYGGINGSPLAN_API_BASEURL } from "../src/constants";
-import {Tabs} from "@navikt/ds-react";
-import {ValgtAktivitet} from "../src/types/ValgtAktivitet";
+import { Tabs } from "@navikt/ds-react";
+import { ValgtAktivitet } from "../src/types/ValgtAktivitet";
 import { Plan } from "../src/components/Plan";
 
 export async function getServerSideProps(context: NextPageContext) {
-    const aktiviteterRespons = await fetch(`${FOREBYGGINGSPLAN_API_BASEURL}/aktivitetsmaler`);
-    const valgteAktiviteterRespons = await fetch(`${FOREBYGGINGSPLAN_API_BASEURL}/valgteaktiviteter/123456789`);
+    const aktiviteterRespons = await fetch(
+        `${FOREBYGGINGSPLAN_API_BASEURL}/aktivitetsmaler`
+    );
+    const valgteAktiviteterRespons = await fetch(
+        `${FOREBYGGINGSPLAN_API_BASEURL}/valgteaktiviteter/123456789`
+    );
     return {
         props: {
             aktiviteter: await aktiviteterRespons.json(),
@@ -20,19 +24,19 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const navigasjonKonstanter = {
-    aktivitetsOversiktTab : {
+    aktivitetsOversiktTab: {
         label: "Oversikt",
-        key: "aktiviteter"
+        key: "aktiviteter",
     },
-    forebyggingsplanTab : {
+    forebyggingsplanTab: {
         label: "Plan",
-        key: "plan"
+        key: "plan",
     },
-    fullførteAktiviteterTab : {
+    fullførteAktiviteterTab: {
         label: "Fullførte aktiviteter",
-        key: "fullførteAktiviteter"
-    }
-}
+        key: "fullførteAktiviteter",
+    },
+};
 
 interface Props {
     aktiviteter: Aktivitet[];
@@ -40,10 +44,10 @@ interface Props {
 }
 
 const Home: NextPage<Props> = ({ aktiviteter, valgteAktiviteter }) => {
-    console.log(aktiviteter)
-    console.log(valgteAktiviteter)
+    console.log(aktiviteter);
+    console.log(valgteAktiviteter);
     return (
-        <div className={styles.container}>
+        <>
             <Head>
                 <title>Forebyggingsplan</title>
                 <meta
@@ -52,27 +56,75 @@ const Home: NextPage<Props> = ({ aktiviteter, valgteAktiviteter }) => {
                 />
                 <link rel="icon" href="https://nav.no/favicon.ico" />
             </Head>
-
-            <main className={styles.main}>
-                <h1 className={styles.title}>Forebyggingsplan</h1>
-                <Tabs defaultValue={navigasjonKonstanter.aktivitetsOversiktTab.key} size="medium">
-                    <Tabs.List>
-                        <Tabs.Tab value={navigasjonKonstanter.aktivitetsOversiktTab.key} label={navigasjonKonstanter.aktivitetsOversiktTab.label}/>
-                        <Tabs.Tab value={navigasjonKonstanter.forebyggingsplanTab.key} label={navigasjonKonstanter.forebyggingsplanTab.label}/>
-                        <Tabs.Tab value={navigasjonKonstanter.fullførteAktiviteterTab.key} label={navigasjonKonstanter.fullførteAktiviteterTab.label}/>
-                    </Tabs.List>
-                    <Tabs.Panel value={navigasjonKonstanter.aktivitetsOversiktTab.key}>
-                        <Aktivitetsmaler aktiviteter={aktiviteter} />
-                    </Tabs.Panel>
-                    <Tabs.Panel value={navigasjonKonstanter.forebyggingsplanTab.key}>
-                        <Plan valgteAktiviteter={valgteAktiviteter} />
-                    </Tabs.Panel>
-                    <Tabs.Panel value={navigasjonKonstanter.fullførteAktiviteterTab.key}>
-                        <p>Her kommer {navigasjonKonstanter.fullførteAktiviteterTab.label}</p>
-                    </Tabs.Panel>
-                </Tabs>
-            </main>
-        </div>
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    <h1 className={styles.title}>Forebyggingsplan</h1>
+                    <Tabs
+                        defaultValue={
+                            navigasjonKonstanter.aktivitetsOversiktTab.key
+                        }
+                        size="medium"
+                    >
+                        <Tabs.List>
+                            <Tabs.Tab
+                                value={
+                                    navigasjonKonstanter.aktivitetsOversiktTab
+                                        .key
+                                }
+                                label={
+                                    navigasjonKonstanter.aktivitetsOversiktTab
+                                        .label
+                                }
+                            />
+                            <Tabs.Tab
+                                value={
+                                    navigasjonKonstanter.forebyggingsplanTab.key
+                                }
+                                label={
+                                    navigasjonKonstanter.forebyggingsplanTab
+                                        .label
+                                }
+                            />
+                            <Tabs.Tab
+                                value={
+                                    navigasjonKonstanter.fullførteAktiviteterTab
+                                        .key
+                                }
+                                label={
+                                    navigasjonKonstanter.fullførteAktiviteterTab
+                                        .label
+                                }
+                            />
+                        </Tabs.List>
+                        <Tabs.Panel
+                            value={
+                                navigasjonKonstanter.aktivitetsOversiktTab.key
+                            }
+                        >
+                            <Aktivitetsmaler aktiviteter={aktiviteter} />
+                        </Tabs.Panel>
+                        <Tabs.Panel
+                            value={navigasjonKonstanter.forebyggingsplanTab.key}
+                        >
+                            <Plan valgteAktiviteter={valgteAktiviteter} />
+                        </Tabs.Panel>
+                        <Tabs.Panel
+                            value={
+                                navigasjonKonstanter.fullførteAktiviteterTab.key
+                            }
+                        >
+                            <p>
+                                Her kommer{" "}
+                                {
+                                    navigasjonKonstanter.fullførteAktiviteterTab
+                                        .label
+                                }
+                            </p>
+                        </Tabs.Panel>
+                    </Tabs>
+                </main>
+            </div>
+        </>
     );
 };
 
