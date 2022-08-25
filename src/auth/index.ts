894261c878aea593041d14d2d37d14655fb817bf
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-08-25 19:00:21 +0200

    Logg headers

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,4 +1,6 @@
 import { IncomingMessage } from "http";
 
-export const hentToken = (req: IncomingMessage) =>
-    req.headers.authorization?.split(" ")[1];
+export const hentToken = (req: IncomingMessage) => {
+    console.log(JSON.stringify(req.headers));
+    return req.headers.authorization?.split(" ")[1];
+};
