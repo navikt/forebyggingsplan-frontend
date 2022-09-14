db67100cf88e1939c027367538e4650f7990b516
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-09-14 17:54:29 +0200

    Oppdater avhengigheter og reformatter

--- src/components/Aktivitetsmaler.tsx
+++ src/components/Aktivitetsmaler.tsx
@@ -1,30 +1,36 @@
-import {Button, Table} from "@navikt/ds-react";
-import {Aktivitet} from "../types/Aktivitet";
+import { Button, Table } from "@navikt/ds-react";
+import { Aktivitet } from "../types/Aktivitet";
 import { AddCircle } from "@navikt/ds-icons";
 
-export const Aktivitetsmaler = ({aktiviteter}: {aktiviteter: Aktivitet[]}) =>
-    <Table size="medium" id={"aktivitetstabell"}>
-        <Table.Header>
-            <Table.Row>
-                <Table.HeaderCell scope="col">
-                    Alle aktiviteter
-                </Table.HeaderCell>
-                <Table.HeaderCell scope="col">
-                    Legg til
-                </Table.HeaderCell>
-            </Table.Row>
-        </Table.Header>
-        <Table.Body>
-            {aktiviteter.map((aktivitet) =>
-                <Table.Row key={aktivitet.tittel}>
-                    <Table.DataCell scope="col">
-                        {aktivitet.tittel}
-                    </Table.DataCell>
-                    <Table.DataCell>
-                        <Button>
-                            <AddCircle/>
-                        </Button>
-                    </Table.DataCell>
-                </Table.Row>)}
-        </Table.Body>
-    </Table>
+export const Aktivitetsmaler = ({
+    aktiviteter,
+}: {
+    aktiviteter: Aktivitet[];
+}) => (
+    <div data-theme="light">
+        <Table id={"aktivitetstabell"} zebraStripes>
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell>Alle aktiviteter</Table.HeaderCell>
+                    <Table.HeaderCell>Legg til</Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+            <Table.Body>
+                {aktiviteter.map((aktivitet) => (
+                    <Table.ExpandableRow
+                        key={aktivitet.tittel}
+                        content={<div>Hei</div>}
+                        togglePlacement="right"
+                    >
+                        <Table.DataCell>{aktivitet.tittel}</Table.DataCell>
+                        <Table.DataCell>
+                            <Button>
+                                <AddCircle />
+                            </Button>
+                        </Table.DataCell>
+                    </Table.ExpandableRow>
+                ))}
+            </Table.Body>
+        </Table>
+    </div>
+);
