d3a533a588ee41838f5e59330f03ab9746757477
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-10-13 15:42:35 +0200

    Cleanup

--- e2e/cypress/e2e/app.cy.ts
+++ e2e/cypress/e2e/app.cy.ts
@@ -20,24 +20,5 @@ describe("Navigasjon", () => {
             cy.findByText("Pilotering av medarbeidersamtalen").should("exist");
             cy.findByText("Alle aktiviteter").should("exist");
         });
-
-        /*cy.get("#aktivitetstabell thead th").should(($headers) => {
-            expect($headers).to.have.length(3);
-            expect($headers.eq(0)).to.contain("Alle aktiviteter");
-            expect($headers.eq(1)).to.contain("Legg til");
-        });
-
-        cy.get("#aktivitetstabell tbody td").should(($datacells) => {
-            expect($datacells).to.have.length(16);
-            expect($datacells.eq(0)).to.contain(
-                "Pilotering av medarbeidersamtalen"
-            );
-            expect($datacells.eq(4)).to.contain("Kartleggingsmøte med ansatt");
-            expect($datacells.eq(8)).to.contain("Sinnemestring");
-            expect($datacells.eq(12)).to.contain(
-                "Hvordan ta den vanskelige praten?"
-            );
-            expect($datacells.eq(1).children(".navds-button")).to.exist;
-        });*/
     });
 });
