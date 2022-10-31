9903adcc7d7e473867a21c365eeac788ce4d0245
Ingrid Fosså <fossa.ingrid@gmail.com>
2022-10-31 12:55:45 +0100

    Legg til styling på kategoriar
    Co-authored-by: Tanet Trimas <tanet.trimas@nav.no>
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

--- src/components/Forebyggingsplan/Aktivitetskategorier.tsx
+++ src/components/Forebyggingsplan/Aktivitetskategorier.tsx
@@ -1,6 +1,7 @@
 import { Accordion, BodyShort, Heading } from "@navikt/ds-react";
 import { Aktivitetsrad } from "./Aktivitetsrad";
 import { Kategori } from "../../types/kategori";
+import styles from "./Aktivitetskategorier.module.css";
 
 interface Props {
     kategorier: Kategori[];
@@ -8,13 +9,13 @@ interface Props {
 
 export const Aktivitetskategorier = ({ kategorier }: Props) => {
     return (
-        <div data-theme="light">
+        <div data-theme="light" className={styles.aktivitetskategorier}>
             {kategorier.map(({aktiviteter, tittel, beskrivelse}) => {
                 return (
-                    <article key={tittel}>
+                    <article key={tittel} className={styles.kategori}>
                         <Heading size="large">{tittel}</Heading>
                         <BodyShort>{beskrivelse}</BodyShort>
-                        <Accordion>
+                        <Accordion className={styles.accordion}>
                             {aktiviteter.map((aktivitet) => {
                                 return (
                                     <Aktivitetsrad
