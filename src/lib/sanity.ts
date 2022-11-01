fadcbc3ae3097b41784a414b56d3a72348f97ef6
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-11-01 11:11:36 +0100

    Legg på apiversion til sanity clienten

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,5 +3,6 @@ import sanityClient from "@sanity/client";
 export const sanity = sanityClient({
     projectId: "2u7e6oll",
     dataset: "production",
+    apiVersion: "2022-10-28",
     useCdn: process.env.NODE_ENV === "production",
 });
