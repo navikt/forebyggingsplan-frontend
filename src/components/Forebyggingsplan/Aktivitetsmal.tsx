a1f982d43c4c2f4f4653579c2a153526eb1ac349
Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
2022-11-08 13:57:56 +0100

    WIP: funksjon for å fullføre en aktivitet
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

--- src/components/Forebyggingsplan/Aktivitetsmal.tsx
+++ src/components/Forebyggingsplan/Aktivitetsmal.tsx
@@ -7,6 +7,7 @@ import { Seksjon } from "../Seksjon/Seksjon";
 import { block } from "../PortableText/block/Block";
 import { marks } from "../PortableText/marks/Marks";
 import { useHentOrgnummer } from "../Layout/Banner/Banner";
+import {aktivitet} from "../../../sanity-studio/schemas/aktivitet";
 
 const hovedinnhold: Partial<PortableTextComponents> = {
     types: {
@@ -29,21 +30,15 @@ export function Aktivitetsmal({
                     <Button
                         variant="secondary"
                         onClick={() => {
-                            fetch("/api/aktivitet", {
-                                method: "POST",
-                                body: JSON.stringify({ id, orgnr }),
-                                headers: {
-                                    "Content-Type": "application/json",
-                                },
-                            }).then((res) => {
-                                return res.json();
-                            });
+                            orgnr && velgAktivitet({ aktivitetsmalId: id, orgnr: orgnr })
                         }}
                     >
                         Dette vil vi gjøre
                     </Button>
                 )}
-                <Button variant="secondary">
+                <Button variant="secondary" onClick={() => {
+                    orgnr && fullførAktivitet({ aktivitetsId: Number.POSITIVE_INFINITY, orgnr: orgnr})
+                }}>
                     {status === "VALGT" ? "Ferdig" : "Dette har vi på plass"}
                 </Button>
             </span>
@@ -57,3 +52,43 @@ export function Aktivitetsmal({
         </div>
     );
 }
+
+
+interface ValgtAktivitetDTO {
+    aktivitetsmalId: string;
+    orgnr: string;
+}
+
+function velgAktivitet(valgtAktivitetDto: ValgtAktivitetDTO) {
+    return fetch("/api/aktivitet", {
+        method: "POST",
+        body: JSON.stringify({ id: valgtAktivitetDto.aktivitetsmalId, orgnr: valgtAktivitetDto.orgnr }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then((res) => {
+        return res.json();
+    });
+}
+
+
+interface FullførAktivitetDTO {
+    aktivitetsId: number;
+    orgnr: string;
+}
+
+function fullførAktivitet(fullførAktivitetDto: FullførAktivitetDTO) {
+    return fetch("/api/aktivitet", {
+        method: "POST",
+        body: JSON.stringify({
+            aktivitetsId: fullførAktivitetDto.aktivitetsId,
+            orgnr: fullførAktivitetDto.orgnr
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then((res) => {
+        return res.json();
+    });
+}
+
