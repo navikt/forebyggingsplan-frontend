017a1cd8d34bffdebb4a0c6f6cdcdb93218b9b62
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2022-11-10 15:33:39 +0100

    Lint feil.

--- src/pages/api/aktivitet.ts
+++ src/pages/api/aktivitet.ts
@@ -12,16 +12,19 @@ export default async function handler(
     const baseUrl = process.env.FOREBYGGINGSPLAN_API_BASEURL;
     const token = await hentTokenXToken(req, res);
 
-    const respons = await fetch(`${baseUrl}/valgteaktiviteter/${req.body.orgnr}`, {
-        method: "POST",
-        body: JSON.stringify(body),
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-        },
-    }).then((res) => {
-        if (res.ok) return {status: res.status, body: res.json()}
-        return {status: res.status, body: res.text()}
+    const respons = await fetch(
+        `${baseUrl}/valgteaktiviteter/${req.body.orgnr}`,
+        {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        }
+    ).then((res) => {
+        if (res.ok) return { status: res.status, body: res.json() };
+        return { status: res.status, body: res.text() };
     });
 
     res.status(respons.status).send(respons.body);
