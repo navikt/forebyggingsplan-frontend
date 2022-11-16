eeaa857addb8c6b9a5ee68d3749f9f6db2d61c06
Christian Klem <christian.klem@nav.no>
2022-11-16 17:26:19 +0100

    Gjør sykefraværstatsitikk api litt mer robust
    - trekk ut audience for sykefraværsstatistikk-api til miljøvariabel

--- src/pages/api/sykefravarsstatistikk.ts
+++ src/pages/api/sykefravarsstatistikk.ts
@@ -6,10 +6,15 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    if (!req.query.orgnr)
+        return res.status(400).json({ error: "Mangler parameter 'orgnr'" });
+    if (req.method !== "GET")
+        return res.status(405).json({ error: "Method Not Allowed" });
+
     const token = await hentTokenXToken(
         req,
         res,
-        "dev-fss:arbeidsgiver:sykefravarsstatistikk-api"
+        process.env.SYKEFRAVÆRSSTATISTIKK_API_CLIENT_ID
     );
     const data = await fetch(
         `https://sykefravarsstatistikk-api.dev-fss-pub.nais.io/sykefravarsstatistikk-api/${req.query.orgnr}/v1/sykefravarshistorikk/aggregert`,
