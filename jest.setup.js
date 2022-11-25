dab2e283d9ce5a2019db60844698243f9a338f3c
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-11-25 18:45:46 +0100

    Oppdater til ts-filer

--- jest.setup.js
+++ jest.setup.js
@@ -5,7 +5,7 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
 import { toHaveNoViolations } from "jest-axe";
-import { server } from "./src/mocks/server";
+import { server } from "./src/mocks/msw";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { fetch } from "whatwg-fetch"; // Trenger denne for å få fetch til å funke i tester
 
