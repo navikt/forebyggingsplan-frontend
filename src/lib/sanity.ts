02f7e050ba75ad6f8517234c68e373523f762b80
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-11-29 10:03:30 +0100

    La til et nytt dataset for lokal utvikling (development)
    Ved deploy så vil produksjonsdatasettet brukes sÃpå naismiljøene så kjøres det mot prod-datasetet til sanitty

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,7 +2,8 @@ import sanityClient from "@sanity/client";
 
 export const sanity = sanityClient({
     projectId: "2u7e6oll",
-    dataset: "production",
+    dataset:
+        process.env.NODE_ENV === "production" ? "production" : "development",
     apiVersion: "2022-10-28",
     useCdn: process.env.NODE_ENV === "production",
 });
