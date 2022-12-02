5cb6a91c282925bf19762b76a729138afb3882de
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-12-02 11:58:36 +0100

    Legg til tester for statistikk-panelet

---
+++ src/components/StatistikkPanel/StatistikkPanel.test.tsx
@@ -0,0 +1,64 @@
+import { render, screen } from "@testing-library/react";
+import { StatistikkPanel } from "./StatistikkPanel";
+import { axe } from "jest-axe";
+
+describe("StatistikkPanel", () => {
+    it("Har ingen axe feil", async () => {
+        const { container } = render(
+            <StatistikkPanel
+                sykefravær={"10"}
+                tittel={"Sykefravær i virksomhet"}
+                tooltip="Din virksomhet"
+            />
+        );
+        const results = await axe(container);
+        expect(results).toHaveNoViolations();
+    });
+
+    it("Viser 'uendret trend' når trend ikke er gitt til komponenten", async () => {
+        render(
+            <StatistikkPanel
+                sykefravær={"10"}
+                tittel={"Sykefravær i virksomhet"}
+                tooltip="Din virksomhet"
+            />
+        );
+        expect(
+            screen.getByRole("img", {
+                name: /uendret trend/i,
+            })
+        ).toBeInTheDocument();
+    });
+
+    it("Viser stigende trend", async () => {
+        render(
+            <StatistikkPanel
+                sykefravær={"10"}
+                tittel={"Sykefravær i virksomhet"}
+                tooltip="Din virksomhet"
+                trend={"2"}
+            />
+        );
+        expect(
+            screen.getByRole("img", {
+                name: /stigende trend/i,
+            })
+        ).toBeInTheDocument();
+    });
+
+    it("Viser synkende trend", async () => {
+        render(
+            <StatistikkPanel
+                sykefravær={"10"}
+                tittel={"Sykefravær i virksomhet"}
+                tooltip="Din virksomhet"
+                trend={"-2"}
+            />
+        );
+        expect(
+            screen.getByRole("img", {
+                name: /synkende trend/i,
+            })
+        ).toBeInTheDocument();
+    });
+});
