8412035e484ee000b34180b0fdb6c999b5fd235f
Ingrid Fosså <fossa.ingrid@gmail.com>
2022-12-05 12:17:12 +0100

    Stylar preview av oppgave
    Co-authored-by: Christian Klem <christian.klem@nav.no>

--- sanity-studio/objects/Oppgave.tsx
+++ sanity-studio/objects/Oppgave.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { CSSProperties } from "react";
+import styles from "./oppgavePeview.module.css";
 
 interface Props {
     value: {
@@ -7,27 +7,10 @@ interface Props {
     };
 }
 
-const oppgaveStyle: CSSProperties = {
-    display: "flex",
-    flexDirection: "column",
-    gap: "1rem",
-    padding: "1rem",
-    backgroundColor: "lightblue",
-    borderRadius: "4px",
-};
-
-const oppgaveTagStyle: CSSProperties = {
-    padding: "0.25rem",
-    backgroundColor: "lighblue",
-    border: "1px solid blue",
-    borderRadius: "4px",
-    width: "max-content",
-};
-
 export const Oppgave = ({ value: { tekst } }: Props) => {
     return (
-        <div style={oppgaveStyle}>
-            <div style={oppgaveTagStyle}>Oppgave</div>
+        <div className={styles.oppgaveContainer}>
+            <div className={styles.tag}>Oppgave</div>
             {tekst}
         </div>
     );
