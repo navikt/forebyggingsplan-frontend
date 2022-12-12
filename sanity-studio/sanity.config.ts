d8a4ae734724831f958d05a092b8eed2a2282db8
Christian Klem <christian.klem@nav.no>
2022-12-12 14:47:19 +0100

    Rydd opp i config ifbm oppgradering til sanity studio v3
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>

--- sanity-studio/sanity.config.ts
+++ sanity-studio/sanity.config.ts
@@ -1,15 +1,21 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {schemaTypes} from './schemas/schema'
-import {visionTool} from '@sanity/vision'
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { schemaTypes } from "./schemas/schema";
+import { visionTool } from "@sanity/vision";
 
 export default defineConfig({
-  name: 'default',
-  title: 'forebyggingsplan-pia',
-  projectId: '2u7e6oll',
-  dataset: 'development',
-  plugins: [deskTool(), visionTool()],
-  schema: {
-    types: schemaTypes,
-  },
-})
+    title: "forebyggingsplan-pia",
+    projectId: "2u7e6oll",
+    dataset: "development",
+    plugins: [deskTool(), visionTool()],
+    schema: {
+        types: schemaTypes,
+    },
+    tools: (prev) => {
+        // 👇 Uses environment variables set by Vite in development mode
+        if (import.meta.env.DEV) {
+            return prev;
+        }
+        return prev.filter((tool) => tool.name !== "vision");
+    },
+});
