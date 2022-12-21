6a901f88f53b432b10e475866bad3f9fef825829
Christian Klem <christian.klem@nav.no>
2022-12-21 13:52:02 +0100

    Bruk fetch for å hente sanity data i labs
    sanity klienten fungerer ikke bra sammen med mock service worker, så vi hijacker den når vi kjører i labs
    
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,9 +1,26 @@
 import sanityClient from "@sanity/client";
+import { KategoriDokument } from "../pages";
 
-export const sanity = sanityClient({
+interface SanityCli {
+    fetch: (query: string) => Promise<KategoriDokument[]>;
+}
+
+const sanityConfig = {
     projectId: "2u7e6oll",
     dataset:
         process.env.NODE_ENV === "production" ? "production" : "development",
     apiVersion: "2022-10-28",
     useCdn: process.env.NODE_ENV === "production",
-});
+};
+export const sanity: SanityCli = sanityClient(sanityConfig);
+
+export const sanityLabs: SanityCli = {
+    fetch: (query) =>
+        fetch(
+            `https://${sanityConfig.projectId}.api.sanity.io/v${
+                sanityConfig.apiVersion
+            }/data/query/production?query=${encodeURIComponent(query)}`
+        )
+            .then((res) => res.json())
+            .then((res) => res.result),
+};
