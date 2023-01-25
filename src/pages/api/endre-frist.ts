a36157b396e8d8f5077119002358b9046e3a2e14
Thomas Dufourd <thomas.dufourd@gmail.com>
2023-01-25 16:30:53 +0100

    Oppdater first dato på aktivitet ved 'endre frist' -- api kall
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

---
+++ src/pages/api/endre-frist.ts
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { hentTokenXToken } from "../../auth/hentTokenXToken";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (!req.body.orgnr)
+        return res.status(400).json({ error: "Mangler 'orgnr' i body" });
+
+    const requestBody = JSON.parse(
+        JSON.stringify({
+            aktivitetsId: req.body.aktivitetsId,
+            aktivitetsmalId: req.body.aktivitetsmalId,
+            frist: req.body.frist,
+        })
+    );
+    const baseUrl = process.env.FOREBYGGINGSPLAN_API_BASEURL;
+
+    let token;
+    try {
+        token = await hentTokenXToken(
+            req,
+            process.env.FOREBYGGINGSPLAN_CLIENT_ID
+        );
+    } catch (e) {
+        return res.status(401).end();
+    }
+
+    const respons = await fetch(
+        `${baseUrl}/valgteaktiviteter/${req.body.orgnr}/endre-frist`,
+        {
+            method: "POST",
+            body: JSON.stringify(requestBody),
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        }
+    );
+    const { status, responseBody } = {
+        status: respons.status,
+        responseBody: respons.ok ? await respons.json() : await respons.text(),
+    };
+    res.status(status).json(responseBody);
+}
