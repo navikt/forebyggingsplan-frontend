bf7e6412851c2e3305da43a88805083f6afb8060
Thomas Dufourd <thomas.dufourd@gmail.com>
2023-02-14 15:51:19 +0100

    Fiks: url til innlogget mottat ia-tjenester metrikker

--- src/pages/api/ia-metrikker.ts
+++ src/pages/api/ia-metrikker.ts
@@ -32,14 +32,17 @@ export default async function handler(
         return res.status(401).end();
     }
 
-    const data = await fetch(`${process.env.IA_METRIKKER_API_BASEURL}`, {
-        method: "POST",
-        body: JSON.stringify(requestBody),
-        headers: {
-            "Content-Type": "application/json",
-            authorization: `Bearer ${token}`,
-        },
-    })
+    const data = await fetch(
+        `${process.env.IA_METRIKKER_API_BASEURL}/innlogget/mottatt-iatjeneste`,
+        {
+            method: "POST",
+            body: JSON.stringify(requestBody),
+            headers: {
+                "Content-Type": "application/json",
+                authorization: `Bearer ${token}`,
+            },
+        }
+    )
         .then((res) => res.json())
         .catch(logger.warn);
 
