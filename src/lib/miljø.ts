a6c1d9eefefd727f35722851c2a140ec368f6e4c
Christian Klem <christian.klem@nav.no>
2023-02-23 10:56:35 +0100

    Fiks sjekk for miljø
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>
    Co-authored-by: Ingrid Fosså <ingrid.fossa@nav.no>

--- src/lib/miljø.ts
+++ src/lib/miljø.ts
@@ -4,4 +4,5 @@ type Miljø = "labs-gcp" | "prod-gcp" | "dev-gcp" | "localhost";
 export const isMock = () =>
     (process.env.NAIS_APP_NAME as AppName) === "forebyggingsplan-frontend-mock";
 
-export const isProd = () => (process.env.NAIS_APP_NAME as Miljø) === "prod-gcp";
+export const isProd = () =>
+    (process.env.NAIS_CLUSTER_NAME as Miljø) === "prod-gcp";
