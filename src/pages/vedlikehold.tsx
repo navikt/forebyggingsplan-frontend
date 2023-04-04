045fefbbc11865158850179842206e9247c88937
Thomas Dufourd <thomas.dufourd@gmail.com>
2023-04-04 09:38:55 +0200

    Vedlikehold vindu mens vi driver med flytting av applikasjon til teamia
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>

---
+++ src/pages/vedlikehold.tsx
@@ -0,0 +1,16 @@
+import { BodyLong } from "@navikt/ds-react";
+import styles from "./feilsider.module.css";
+
+export default function Vedlikehold() {
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>
+                <h1>Denne siden er ikke tilgjengelig for øyeblikket</h1>
+                <BodyLong className={styles.body}>
+                    Vi jobber med å gjøre denne siden bedre. Vennligst prøv
+                    igjen senere.
+                </BodyLong>
+            </main>
+        </div>
+    );
+}
