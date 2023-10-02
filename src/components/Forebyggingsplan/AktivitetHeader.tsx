e24375d0a75225e53431b8c21aa14880c2588e69
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2023-10-02 10:13:44 +0200

    Drar progress bar til venstre om den wrapper til ny linje.

--- src/components/Forebyggingsplan/AktivitetHeader.tsx
+++ src/components/Forebyggingsplan/AktivitetHeader.tsx
@@ -2,6 +2,7 @@ import { Aktivitet } from "../../types/Aktivitet";
 import { ProgressBar } from "../ProgressBar/ProgressBar";
 import { getAktivitetStatusBeskrivelseTekst } from "./Aktivitetsmal/AktivitetsstatusBeskrivelse";
 import { AktivitetStatistikkType } from "./useAktivitetStatistikk";
+import styles from "./AktivitetHeader.module.css";
 
 export const AktivitetHeader = ({
     aktivitet,
@@ -19,6 +20,7 @@ export const AktivitetHeader = ({
                 partial={aktivitetStatistikk.påbegynte}
                 title={getAktivitetStatusBeskrivelseTekst(aktivitetStatistikk)}
                 navn={aktivitet.tittel}
+                className={styles["aktivitet-header-progress-bar"]}
             />
         </>
     );
